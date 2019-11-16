67a5705999cfeb6d21dbeb2aefb40cc0425acd84
Joey Dumont <joey.dumont@gmail.com>
2019-11-16 16:14:32 -0500

    linting

--- src/handlers/resizes_images.js
+++ src/handlers/resizes_images.js
@@ -1,21 +1,23 @@
 // Create clients outside of the handler
 
+
 // Create a client to read objects from S3
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
+
 // Libraries for image resize.
-const stream = require('stream')
-const sharp = require('sharp')
+const stream = require('stream');
+const sharp = require('sharp');
 
 // Function to create a read stream to an S3 object.
 const readStreamFromS3 = ({Bucket, Key}) => {
-    return s3.getObject({Bucket, Key}).createReadStream()
-}
+    return s3.getObject({Bucket, Key}).createReadStream();
+};
 
 // Function to create a write stream to S3.
 const writeStreamToS3 = ({Bucket, Key}) => {
-    const pass = new stream.PassThrough()
+    const pass = new stream.PassThrough();
     return {
         writeStream: pass,
         uploadFinished: s3.upload({
@@ -24,26 +26,26 @@ const writeStreamToS3 = ({Bucket, Key}) => {
             ContentType: 'image/jpeg',
             Key
         }).promise()
-    }
-}
+    };
+};
 
 // Get size from stream (sharp)
 const getSizeFromStream = () => {
-    const pass = new stream.PassThrough()
-    const metadata = sharp.metadata()
+    const pass = new stream.PassThrough();
+    const metadata = sharp.metadata();
     return {
         writeStream: pass,
         width: metadata.width,
         height: metadata.height
-    }
-}
+    };
+};
 
 // Resize image stream (sharp).
 const resizeStream = ({width, height}) => {
     return sharp()
         .resize(width, height)
-        .toFormat('jpeg')
-}
+        .toFormat('jpeg');
+};
 
 /**
   * A Lambda function that logs the payload received from S3.
@@ -57,65 +59,65 @@ exports.resizeImagesHandler = async (event, context) => {
 
         try {
             // First, copy the original to a new file.
-            fileExt = record.s3.object.key.split('.').pop()
-            splitKey = record.s3.object.key.split('/')
-            prefix = splitKey.splice(splitKey.len()-1, 0, "orig")
-            newKey = prefix.join('/')
+            const fileExt = record.s3.object.key.split('.').pop();
+            const splitKey = record.s3.object.key.split('/');
+            const prefix = splitKey.splice(splitKey.len()-1, 0, "orig");
+            const newKey = prefix.join('/');
 
             // Show keys and stuff.
-            console.log('File extension: ' + fileExt)
-            console.log('Split key:' + splitKey)
-            console.log('prefix:' + prefix)
-            console.log('newKey: ' + newKey)
-            const readStreamOrig = readStreamFromS3(params)
+            console.log('File extension: ' + fileExt);
+            console.log('Split key:' + splitKey);
+            console.log('prefix:' + prefix);
+            console.log('newKey: ' + newKey);
+            const readStreamOrig = readStreamFromS3(params);
             const {
                 passThroughStream,
                 origWidth,
                 origHeight
-            } = getSizeFromStream()
+            } = getSizeFromStream();
             const {
                 writeStreamOrig,
                 uploadFinishedOrig
-            } = writeStreamToS3(Bucket: params[0], Key: newKey)
+            } = writeStreamToS3({Bucket: params[0], Key: newKey});
 
             readStreamOrig
                 .pipe(passThroughStream)
-                .pipe(writeStreamOrig)
+                .pipe(writeStreamOrig);
 
-            const uploadedData = await uploadFinishedOrig
+            const uploadedData = await uploadFinishedOrig;
 
-            console.log("Original width: " + origWidth)
-            console.log("Original height: " + origHeight)
+            console.log("Original width: " + origWidth);
+            console.log("Original height: " + origHeight);
 
             console.log('Image was reuploaded', {
                 ...uploadedData
-            })
+            });
 
             // Have to determine the original width and height.
             // Can that be done through a stream, or does it have to be done
             // before processing?
             // Compute ratio of width/height. width will always be 500 pixels in the end.
-            const ratio = origWidth / origHeight
-            const readSTream = readStreamFromS3({Bucket: params[0], Key: newKey})
-            const resizeStream = resizeStream({500, 500/ratio})
+            const ratio = origWidth / origHeight;
+            const readStream = readStreamFromS3({Bucket: params[0], Key: newKey});
+                        const resizeStreamVar = resizeStream({width: 500, height: 500/ratio});
             const {
                 writeStream,
                 uploadFinished
-            } = writeStreamToS3({Bucket: params[0], Key: params[1]})
+            } = writeStreamToS3({Bucket: params[0], Key: params[1]});
 
             // Trigger the stream.
             readStream
-                .pipe(resizeStream)
-                .pipe(writeStream)
+                .pipe(resizeStreamVar)
+                .pipe(writeStream);
 
-            const uploadedData = await uploadFinished
+            const uploadedDataResized = await uploadFinished;
 
             console.log('Image was resized:', {
-                ...uploadedData
-            })
+                ...uploadedDataResized
+            });
 
         } catch (error) {
-            console.log('Error calling S3 getObject: ' + error)
+            console.log('Error calling S3 getObject: ' + error);
             console.error('Error calling S3 getObject:' + error);
             throw error;
         }
