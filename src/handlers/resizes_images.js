5039e0c057ccb84f1095b8944031efe054340d2a
Joey Dumont <joey.dumont@gmail.com>
2019-11-27 22:28:40 -0500

    console.log everywhere.

--- src/handlers/resizes_images.js
+++ src/handlers/resizes_images.js
@@ -12,12 +12,14 @@ const s3s = require('s3-streams')
 const stream = require('stream');
 const sharp = require('sharp');
 
+AWS.config.logger = console.log
+
 // Function to create a read stream to an S3 object.
 const readStreamFromS3 = ({Bucket, Key}) => {
     //return s3.getObject({Bucket, Key}).createReadStream();
     return s3s.ReadStream(s3, {
         Bucket,
-        Key
+        Key,
     })
 };
 
@@ -35,7 +37,8 @@ const writeStreamToS3 = ({Bucket, Key}) => {
     //};
     return s3s.WriteStream(s3, {
         Bucket,
-        Key
+        Key,
+        ContentType: 'image/jpeg',
     })
 };
 
@@ -70,18 +73,21 @@ exports.resizeImagesHandler = async (event, context) => {
             //console.log('prefix:' + prefix);
             console.log('newKey: ' + newKey);
             console.log('params', {...params})
+            console.log('params', {...params})
 
 
             // Copy the object to another location.
+            console.log('Defining read stream.')
             const readStreamOrig = readStreamFromS3(params);
-
             //const {
             //    writeStreamOrig,
             //    uploadFinishedOrig
             //} = writeStreamToS3({Bucket: params[0], Key: newKey})
 
+            console.log('Defining write stream.')
             const writeStreamOrig = writeStreamToS3({Bucket: params[0], Key: newKey})
 
+            console.log('Initiating the pipe')
             readStreamOrig
                 .pipe(writeStreamOrig);
 
@@ -92,16 +98,20 @@ exports.resizeImagesHandler = async (event, context) => {
             //});
 
             // Resize the image and overwrite the original upload.
+            console.log('Defining read stream for resize')
             const readStream = readStreamFromS3({Bucket: params[0], Key: newKey});
+            console.log('Defining resize stream')
             const resizeStreamVar = resizeStream({width: 500, height: 500});
             //const {
             //    writeStream,
             //    uploadFinished
             //} = writeStreamToS3(params);
-
+            console.log('Defining write stream for resize')
+            console.log('params', {...params})
             const writeStream = writeStreamToS3(params);
 
             // Trigger the stream.
+            console.log('Trigger resize stream')
             readStream
                 .pipe(resizeStreamVar)
                 .pipe(writeStream);
