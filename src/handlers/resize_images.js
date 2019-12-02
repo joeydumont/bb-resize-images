fc315aaa82e7dfd6ec51ff1d25bc397467eda64f
Joey Dumont <joey.dumont@gmail.com>
2019-12-01 21:00:35 -0500

    Linting, and added file format to write stream function.

--- src/handlers/resize_images.js
+++ src/handlers/resize_images.js
@@ -3,10 +3,10 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
     signatureVersion: 'v4'
 });
-AWS.config.logger = console.log
+AWS.config.logger = console.log;
 
 // Libraries for image resize.
-const s3s = require('s3-streams')
+const s3s = require('s3-streams');
 const util = require('util');
 const stream = require('stream');
 const sharp = require('sharp');
@@ -15,24 +15,24 @@ const readStreamFromS3 = ({Bucket, Key}) => {
     return s3s.ReadStream(s3, {
         Bucket: Bucket,
         Key: Key,
-    })
+    });
 };
 
 const writeStreamToS3 = ({Bucket, Key, FileFormat}) => {
     let ContentType;
-    if FileFormat == ".jpg" {
-        ContentType = 'image/jpeg'
+    if (FileFormat == ".jpg") {
+        ContentType = 'image/jpeg';
     }
 
-    else if FileFormat == ".png" {
-        ContentType = 'image/png'
+    else if (FileFormat == ".png") {
+        ContentType = 'image/png';
     }
 
     return s3s.WriteStream(s3, {
         Bucket: Bucket,
         Key: Key,
         ContentType: 'image/jpeg',
-    })
+    });
 };
 
 const resizeStream = ({width, height}) => {
@@ -51,7 +51,7 @@ exports.resizeImagesHandler = async (event, context) => {
             Key: record.s3.object.key,
         };
 
-        const imageType = params['Key'].match(/\.([^.])*$/)[0].toLowerCase();
+        const imageType = params.Key.match(/\.([^.])*$/)[0].toLowerCase();
 
         if (imageType != ".jpg" && imageType != ".png") {
             return;
@@ -59,22 +59,20 @@ exports.resizeImagesHandler = async (event, context) => {
 
         try {
             // Determine the new keys
-            const newKeyMove = 'orig/' + params['Key'];
-
-            const params['Key'].split('.')
+            const newKeyMove = 'orig/' + params.Key;
 
             // Pipeline to copy the original S3 object to a different location.
-            const pipeline = util.promisify(stream.pipeline)
+            const pipeline = util.promisify(stream.pipeline);
             const readStreamOrig = readStreamFromS3(params);
-            const writeStreamOrig = writeStreamToS3({Bucket: params['Bucket'], Key: newKey})
-            await pipeline(readStreamOrig, writeStreamOrig)
+            const writeStreamOrig = writeStreamToS3({Bucket: params.Bucket, Key: newKeyMove, FileFormat: imageType});
+            await pipeline(readStreamOrig, writeStreamOrig);
 
             // Pipeline to resize the original file and write the new in the original upload location.
-            const pipelineResize = util.promisify(stream.pipeline)
-            const readStream = readStreamFromS3({Bucket: params['Bucket'], Key: newKey});
+            const pipelineResize = util.promisify(stream.pipeline);
+            const readStream = readStreamFromS3({Bucket: params.Bucket, Key: newKeyMove});
             const resizeStreamVar = resizeStream({width: 500, height: 500});
-            const writeStream = writeStreamToS3(params);
-            await pipelineResize(readStream, resizeStreamVar, writeStream)
+            const writeStream = writeStreamToS3({Bucket: params.Bucket, Key: params.Key, FileFormat: imageType});
+            await pipelineResize(readStream, resizeStreamVar, writeStream);
 
         } catch (error) {
             console.log('Error : ' + error);
